8ba4cf9a0c632526f97dd84bb16aefb8ad9f5cfb
Quynh Nguyen <qnguyenx3@gmail.com>
2018-05-18 15:41:09 -0700

    WIP create user profile

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.android.userprofile;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView photo;
+    TextView name;
+    TextView birthday;
+    TextView country;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.name);
+        birthday = (TextView) findViewById(R.id.birthday);
+        country = (TextView) findViewById(R.id.country);
+
+        name.setText("Quynh Nguyen");
+        birthday.setText("01/20/1994");
+        country.setText("Philippines");
     }
 }
